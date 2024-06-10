605200b7a48885c202f92e683c8683d79b65ecbe
yudirunat <yudi.berland@gmail.com>
2024-06-10 10:33:55 +0700

    people layout

--- app/components/Features/index.tsx
+++ app/components/Features/index.tsx
@@ -1,56 +1,70 @@
 import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
 
 interface datatype {
-    imgSrc: string;
-    heading: string;
-    paragraph: string;
+  imgSrc: string;
+  heading: string;
+  paragraph: string;
 }
 
 const Aboutdata: datatype[] = [
-    {
-        imgSrc: "/assets/features/time.svg",
-        heading: "Cash collection",
-        paragraph: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa',
-    },
-    {
-        imgSrc: "/assets/features/signal.svg",
-        heading: "Insights",
-        paragraph: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa',
-
-    },
-    {
-        imgSrc: "/assets/features/dollar.svg",
-        heading: "Payment",
-        paragraph: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa',
-
-    }
-]
+  {
+    imgSrc: "/assets/features/time.svg",
+    heading: "Delivery",
+    paragraph:
+      "Manjakan dirimu dengan kue lezat tanpa harus keluar rumah! Kami siap mengantarkan kue favoritmu langsung ke pintu rumahmu. Pesan sekarang dan nikmati kenyamanan delivery! ",
+  },
+  {
+    imgSrc: "/assets/features/signal.svg",
+    heading: "Bahan Terbaik",
+    paragraph:
+      "Kue lezat dengan bahan terjamin dan bebas pengawet! Kami hanya menggunakan bahan berkualitas untuk menghadirkan rasa terbaik tanpa kompromi. Nikmati kue sehat dan enak setiap saat!",
+  },
+  {
+    imgSrc: "/assets/features/dollar.svg",
+    heading: "Harga Bersaing",
+    paragraph:
+      "Tidak perlu mahal untuk menikmati kue lezat dan berkualitas! Nikmati setiap gigitan dengan harga terjangkau. Dijamin enak dan bikin ketagihan!",
+  },
+];
 
 const Features = () => {
-    return (
-        <div className="bg-babyblue" id="features">
-            <div className="mx-auto max-w-2xl py-20 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h3 className="text-4xl sm:text-5xl font-semibold text-black text-center my-10">Amazing Features.</h3>
-                <h5 className="text-black opacity-60 text-lg font-normal text-center">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque <br /> laudantium, totam rem aperiam, eaque ipsa quae ab.</h5>
+  return (
+    <div className="bg-babyblue" id="features">
+      <div className="mx-auto max-w-2xl py-20 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h3 className="text-4xl sm:text-5xl font-semibold text-black text-center my-10">
+          KEUNGGULAN KAMI
+        </h3>
+        <hr
+          style={{ border: "1px solid navy", color: "blue" }}
+          className=" mx-auto w-20  border-1 -mt-2"
+        />
 
-                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-16 gap-y-4 lg:gap-x-8 mt-10'>
-                    {Aboutdata.map((item, i) => (
-                        <div key={i} className='bg-white rounded-2xl p-5 featureShadow'>
-
-                            <Image src={item.imgSrc} alt={item.imgSrc} width={55} height={55} className="mb-2" />
-                            <h3 className="text-2xl font-semibold text-black mt-5">{item.heading}</h3>
-                            <h4 className='text-lg font-normal text-black opacity-50 my-2'>{item.paragraph}</h4>
-                            <Link href={'/'} className="text-electricblue text-xl font-medium flex gap-2 pt-10 pb-2">
-                                Learn more <Image src="/assets/people/arrow-right.svg" alt="arrow-right" width={24} height={24} />
-                            </Link>
-
-                        </div>
-                    ))}
-                </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-16 gap-y-4 lg:gap-x-8 mt-10">
+          {Aboutdata.map((item, i) => (
+            <div key={i} className="bg-white rounded-2xl p-5 featureShadow">
+              <Image
+                src={item.imgSrc}
+                alt={item.imgSrc}
+                width={55}
+                height={55}
+                className="mb-2"
+              />
+              <h3 className="text-2xl font-semibold text-black mt-5">
+                {item.heading}
+              </h3>
+              <h4 className="text-lg font-normal text-black opacity-50 my-2 text-justify">
+                {item.paragraph}
+              </h4>
+              <br />
+              <br />
+              <br />
             </div>
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Features;
--- app/components/People/index.tsx
+++ app/components/People/index.tsx
@@ -4,61 +4,34 @@ import Link from "next/link";
 const People = () => {
   return (
     <div id="product">
-      <div className="mx-auto max-w-7xl px-6">
-        <div className="grid grid-cols-1 lg:grid-cols-12 space-x-1 pt-10 lg:pt-32 lg:mt-20">
-          <div className="col-span-6 flex justify-center">
-            <Image
-              src="/assets/people/testimony.png"
-              alt="nothing"
-              width={1000}
-              height={805}
-            />
-          </div>
+      <div className="mx-auto mt-14 max-w-2xl py-20 px-4 sm:px-6 lg:max-w-7xl lg:px-8 justify-center">
+        <h3 className="text-4xl sm:text-5xl font-semibold text-black text-center my-10">
+          MACAM - MACAM KUE
+        </h3>
+        <hr
+          style={{ border: "1px solid navy", color: "blue" }}
+          className=" mx-auto w-20  border-1 -mt-2"
+        />
 
-          <div className="col-span-6 flex flex-col justify-evenly lg:pl-24 mt-10 lg:mt-0">
-            <h1 className="text-midnightblue text-4xl sm:text-5xl font-semibold text-center lg:text-start lh-143">
-              More than 100k+ people are using our product.
-            </h1>
-            <h3 className="text-black text-lg font-normal text-center lg:text-start lh-173 opacity-75 pt-5 lg:pt-0">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab.
-            </h3>
-            <Link
-              href={"/"}
-              className="text-electricblue text-lg font-medium flex gap-2 mx-auto lg:mx-0 pt-5 lg:pt-0"
-            >
-              Learn more{" "}
-              <Image
-                src="/assets/people/arrow-right.svg"
-                alt="arrow-right"
-                width={24}
-                height={24}
-              />
-            </Link>
-          </div>
-
-          <div className="col-span-6 flex justify-center">
+        <div className="grid grid-cols-3 gap-10 mt-20  ">
+          <div className=" bg-black-500 aspect-square rounded-lg  justify-center">
             <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
                 <Image
+                  className="rounded-t-lg"
                   src="/assets/cake/Resep-Kue-Cucur-Gula-Merah-Anti-Gagal-Dijamin-Legit-Bikin-Ketagihan-1024x678.jpg"
-                  alt="star-image"
-                  width={700}
+                  alt=""
+                  width={800}
                   height={200}
-                  className=""
                 />
               </a>
               <div className="p-5">
                 <a href="#">
                   <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    Noteworthy technology acquisitions 2021
+                    Kue Cucur Gula Merah
                   </h5>
                 </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  Here are the biggest enterprise technology acquisitions of
-                  2021 so far, in reverse chronological order.
-                </p>
+                <br />
                 <a
                   href="#"
                   className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -73,9 +46,94 @@ const People = () => {
                   >
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className=" bg-black-500 aspect-square rounded-lg mt-2  justify-center">
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                <Image
+                  className="rounded-t-lg"
+                  src="/assets/cake/foto_utama_dadar_gulung.jpg"
+                  alt=""
+                  width={800}
+                  height={200}
+                />
+              </a>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Dadar Gulung
+                  </h5>
+                </a>
+                <a
+                  href="#"
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Read more
+                  <svg
+                    className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-black-500 aspect-square rounded-lg">
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                <Image
+                  className="rounded-t-lg"
+                  src="/assets/cake/marbleCake.jpg"
+                  alt=""
+                  width={800}
+                  height={300}
+                />
+              </a>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Marble Cake
+                  </h5>
+                </a>
+                <br />
+                <a
+                  href="#"
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Read more
+                  <svg
+                    className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M1 5h12m0 0L9 1m4 4L9 9"
                     />
                   </svg>
