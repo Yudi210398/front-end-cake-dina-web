d2bfe1720d0cb5f8c3e6921c5088717d34b18481
yudirunat <yudi.berland@gmail.com>
2024-12-18 00:09:23 +0700

    almost finish ganti links

--- app/components/whatsapp/index.tsx
+++ app/components/whatsapp/index.tsx
@@ -4,23 +4,23 @@ import Link from "next/link";
 const WhatsappIcon = () => {
   const encodePesan = encodeURIComponent("Halo Saya Mau Pesan Kue");
   return (
-    <a target="_blank" href={`https://wa.me/6281388155049?text=${encodePesan}`}>
-      <Image
-        src="/assets/logo/was.png"
-        alt="whatsapp"
-        width={"220"}
-        height={"10"}
-        sizes="100vw"
-        className="fixed bottom-4 right-4 z-50 cursor-pointer"
-        style={{
-          width: "100",
-          height: "auto",
-          position: "fixed",
-          zIndex: 1000,
-          cursor: "pointer",
-        }}
-      />
-    </a>
+    <Link
+      href={`https://wa.me/6281388155049?text=${encodePesan}`}
+      target="_blank"
+      rel="noopener noreferrer"
+      passHref
+    >
+      <a>
+        <Image
+          src="/assets/logo/was.png"
+          alt="whatsapp"
+          width={220}
+          height={10}
+          sizes="100vw"
+          className="fixed bottom-4 right-4 z-50 cursor-pointer"
+        />
+      </a>
+    </Link>
   );
 };
 
